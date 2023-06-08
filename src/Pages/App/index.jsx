5cc077c8cbb9580fc58121c779312d9b70622317
Angel Martinez <a_armando97@hotmail.com>
2023-06-08 14:38:29 -0600

    Navbar component created and styles added to links

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SingIn'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
@@ -26,6 +27,7 @@ const App = () => {
   return (
      <BrowserRouter>
       <AppRoutes />
+      <Navbar />
      </BrowserRouter>   
   )
 }
