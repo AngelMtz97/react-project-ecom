80bde6bd5723117e69c2ead55cf60d3612578bec
Angel Martinez <a_armando97@hotmail.com>
2023-06-15 23:30:12 -0600

    Get data from platzi API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,36 @@
+import {useState, useEffect} from 'react';
 import React from 'react';
 import Card from '../../Components/Card';
 
 const Home = () => {
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then((response)=>{
+           return response.json();
+        })
+        .then((result) => {
+           // console.log(result)
+            setItems(result)
+        })
+
+
+    }, []);
+
     return (
             <div>
                 Home
-                <Card />
+                <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                {
+                    items?.map((item)=>{
+                       return <Card key={item.id} {...item}/>
+                    })
+                }
+                </div>
+               
+               
             </div>
             
     );
