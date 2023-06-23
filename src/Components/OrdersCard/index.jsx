10915ef92ac7a5202e603a392d76e6c972fe5f4d
Angel Martinez <a_armando97@hotmail.com>
2023-06-23 12:21:04 -0600

    My Order and My Orders component updated to show all orders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,5 +1,3 @@
-import {TrashIcon} from '@heroicons/react/24/outline'
-
 
 const OrdersCard = (props) => {
     const {totalPrice, totalProducts} = props;
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import {OrdersCard} from '../../Components/OrdersCard'
+import { Link } from 'react-router-dom';
+import {useContext} from 'react';
+import OrdersCard from '../../Components/OrdersCard';
+import { ShoppingCartContext } from '../../Context';
 
 const MyOrders = () => {
+    
+    const context = useContext(ShoppingCartContext);
+
     return (
         <div>
-            My Orders
-            <OrdersCard />
+            <div className='flex w-80 items-center relative justify-center'>
+                <h1>My Orders</h1>
+            </div>
+            
+            {
+                context.order.map((item, index)=>{
+                    return <Link key={index} to={`/my-orders/${item.id}`}>
+                            <OrdersCard totalPrice={item.totalPrice} totalProducts={item.totalProducts} />
+                          </Link>
+                })
+            }
+           
         </div>
     );
 };
