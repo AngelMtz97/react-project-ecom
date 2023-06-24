abc5cdf60297a4b8d1a52748e7f1fef90489686d
Angel Martinez <a_armando97@hotmail.com>
2023-06-23 20:38:20 -0600

    Orders card component updated

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,14 +1,20 @@
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 const OrdersCard = (props) => {
     const {totalPrice, totalProducts} = props;
 
     return (
-        <div className="flex flex-col justify-between items-center mb-3 border border-gray-500 w-80 p-4 rounded-lg mb-4">
-            <p className="flex flex-col items-center">
-                Date: <span>22.02.23</span>
-                Total items: <span>{totalProducts}</span>
-                Total amount: <span>{totalPrice}</span>
-            </p>
+        <div className='flex flex-col justify-between items-center mb-3 border border-gray-500 w-80 p-4 rounded-lg'>
+            <div className='flex justify-between w-full'>
+                <p className='flex flex-col'>
+                Date: <span className='font-light'>22.02.23</span>
+                Total items: <span className='font-light'>{totalProducts}</span>
+                </p>
+               <p className='flex items-center gap-2'>
+                Total amount: <span className='font-medium text-2xl'>${totalPrice}</span>
+                  <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'/>
+               </p>
+            </div>
         </div>
     );
 };
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,7 +10,7 @@ const MyOrders = () => {
     return (
         <div>
             <div className='flex w-80 items-center relative justify-center'>
-                <h1>My Orders</h1>
+                <h1 className='font-medium text-xl'>My Orders</h1>
             </div>
             
             {
